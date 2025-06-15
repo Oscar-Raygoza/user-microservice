a47835224ddfbc1cf4a234148fefb80fe06f0406
Oscar Eduardo Raygoza <oscar.eduardo.raygoza@gmail.com>
2025-06-14 18:18:30 -0600

    fix: add update on deleate users

--- src/repository/user.repository.ts
+++ src/repository/user.repository.ts
@@ -95,7 +95,7 @@ export default class UserRepository {
 
     const query = `
       UPDATE users SET ${setClause}, updated_at = CURRENT_TIMESTAMP
-      WHERE id = ? AND deleted_at IS NULL;
+      WHERE id = ?;
     `;
     const updateParams = [...params, id];
 
